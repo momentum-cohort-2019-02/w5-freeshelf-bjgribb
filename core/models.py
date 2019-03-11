30ac742cd988c7c226b45bd25632968f55ca4b8c
bjgribb <bjgribb@gmail.com>
2019-03-11 18:43:48 -0400

    Book Model, Author

--- core/models.py
+++ core/models.py
@@ -4,9 +4,19 @@ from django.db import models
 
 class Book(models.Model):
     """Model representing a book"""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    book_url = models.URLField(max_length=200, null=True)
+    date_added = models.DateField('Date Added',auto_now_add=True)
+
 
     def __str__(self):
         """String representation."""
         return self.title
 
+class Author(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=True)
+
+    def __str__(self):
+        return self.name
