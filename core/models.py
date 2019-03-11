500db54b4c62fb30ce412645891edcee14d44188
bjgribb <bjgribb@gmail.com>
2019-03-11 12:54:58 -0400

    Book model started, superuser created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    """Model representing a book"""
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String representation."""
+        return self.title
+
